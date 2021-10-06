b42f613db8358df1d73ddd2720906c9fc4340492
Jennifer Spagnolo <jenniferspagnolo@Jennifers-MBP.lan>
2021-10-05 20:16:36 -0400

    try raw sql

--- create_.py
+++ create_.py
@@ -5,10 +5,28 @@ db.create_all()
 
 #db.CategoryModel.add(['Roof','Landscaping','Electric','Plumbing','Trim','Paint','Flooring'])
 
-categories = ['Roof','Landscaping','Electric','Plumbing','Trim','Paint','Flooring']
-cat_models = []
-for cat in categories:
-    cat_models.append(CategoryModel(cat))
+# categories = ['Roof','Landscaping','Electric','Plumbing','Trim','Paint','Flooring']
+# cat_models = []
+# for cat in categories:
+#     cat_models.append(CategoryModel(cat))
 
-db.session.add(cat_models)
+query = \
+"""
+insert into category
+values 
+(1,'Flooring'),
+(2,'Landscaping'),
+(3,'Electric'),
+(4,'Plumbing'),
+(5,'Paint'),
+(6,'Trim'),
+(7,'Roof'),
+(8,'Doors'),
+(9,'Siding'),
+(10,'Kitchen'),
+(11,'Bathroom'),
+(12,'HVAC')
+
+"""
+db.session.execute(query)
 db.session.commit()
