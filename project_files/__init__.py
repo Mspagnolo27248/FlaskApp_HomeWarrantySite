1f2cb0d379cd3960a874e8b2eb8f1140cdcff5ad
Jennifer Spagnolo <jenniferspagnolo@Jennifers-MBP.lan>
2021-10-09 15:22:57 -0400

    bootsrap and congif

--- project_files/__init__.py
+++ project_files/__init__.py
@@ -12,15 +12,15 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #Heroku Fix
-# uri = os.getenv("DATABASE_URL")  # or other relevant config var
-# if uri.startswith("postgres://"):
-#     uri = uri.replace("postgres://", "postgresql://", 1)
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
 #app congigurations
 app.config['SECRET_KEY'] = 'Lorenzo93'
-#app.config['SQLALCHEMY_DATABASE_URI'] =  uri
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] =  uri
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
